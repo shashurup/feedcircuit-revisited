2caae8a1e52f379056edb81dde792f2a5d5f3a92
Georgy Kibardin <georgy@kibardin.name>
2019-09-04 17:12:26 +0300

    Continue to 0.3.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject feedcircuit-revisited "0.3.3"
+(defproject feedcircuit-revisited "0.3.4-SNAPSHOT"
   :description "Old friend Feedcircuit revisited"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
