a8ae885071da1b1ca03eebbd5a013a1319150352
Georgy Kibardin <georgy@kibardin.name>
2020-02-16 19:59:31 +0300

    Release 0.5.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject feedcircuit-revisited "0.4.6-SNAPSHOT"
+(defproject feedcircuit-revisited "0.5.0"
   :description "Old friend Feedcircuit revisited"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
