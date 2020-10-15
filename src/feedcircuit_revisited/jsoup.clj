ff18eece2535d6654d3d13d0cab942d6c0aa9def
Georgy Kibardin <georgy@kibardin.name>
2020-10-15 20:43:31 +0300

    Ommit absent attributes in the parsed HTML
    Do not insert nil when there are no attributes. This mitigates hiccup
    problem with generating opening and closing tags when at least
    something is specified in attributes

--- src/feedcircuit_revisited/jsoup.clj
+++ src/feedcircuit_revisited/jsoup.clj
@@ -11,25 +11,32 @@
   Document
   (as-clojure [doc]
     (-> doc .children first as-clojure))
+
   Element
   (as-clojure [element]
-    (into
-     [(-> element .tagName keyword)
-      (-> element .attributes as-clojure not-empty)]
-     (->> element .childNodes (map as-clojure) (remove nil?))))
+    (let [tag (-> element .tagName keyword)
+          attrs (-> element .attributes as-clojure not-empty)]
+      (into
+       (if attrs [tag attrs] [tag])
+       (->> element .childNodes (map as-clojure) (remove nil?)))))
+
   Attributes
   (as-clojure [attrs]
     (into {} (map as-clojure attrs)))
+
   Attribute
   (as-clojure [attr]
     [(keyword (.getKey attr))
      (.getValue attr)])
+
   TextNode
   (as-clojure [text-node]
     (.getWholeText text-node))
+
   DataNode
   (as-clojure [data-node]
     (.getWholeData data-node))
+
   Comment
   (as-clojure [comment] nil))
 
