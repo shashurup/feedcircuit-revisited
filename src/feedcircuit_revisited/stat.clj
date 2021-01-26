72f1c2aeb3427f15f7618bf4d4f720def59fc267
Georgy Kibardin <georgy@kibardin.name>
2021-01-26 10:44:35 +0300

    Content to summary ratio statistics
    The new statistics is inteded for figuring self-conatining rss feeds

--- src/feedcircuit_revisited/stat.clj
+++ src/feedcircuit_revisited/stat.clj
@@ -13,45 +13,66 @@
       :class (= (count (content/find-by-identifier ident html)) 1)
       false)))
 
-(defn get-content-identifier [url]
-  (when-let [zipper (->> url
-                         content/retrieve-and-parse
-                         content/find-content-element)]
-    (let [ident (content/get-identifier (zip/node zipper))]
-      (if (unique? ident (zip/root zipper))
-        ident))))
+(defn find-content [item]
+  [item
+   (->> (:link item)
+        content/retrieve-and-parse
+        content/find-content-element)])
 
-(defn value [x] x)
+(defn get-content-identifier [[_ zipper]]
+  (let [ident (content/get-identifier (zip/node zipper))]
+    (when (unique? ident (zip/root zipper))
+      ident)) )
+
+(defn compute-content-identifier [data]
+  (->> data
+       (map get-content-identifier)
+       frequencies
+       (apply max-key second)
+       first))
+
+(defn calc-ratio [[item zipper]]
+  (float (/ (content/text-size-recursively (zip/node zipper))
+            (content/calculate-size (:summary item)))))
+
+(defn average [xs]
+  (when (not-empty xs)
+    (/ (apply + xs) (count xs))))
+
+(defn compute-content-to-summary-ratio [data]
+  (->> data
+       (filter (comp :summary first))
+       (map calc-ratio)
+       average))
 
 (defn compute [url]
-  (let [[_ items] (feed/fetch-items url)]
-    (->> items
-         (map :link)
-         (map #(future (get-content-identifier %)))
-         (map deref)
-         (group-by value)
-         (map (fn [[k v]] [k (count v)]))
-         (apply max-key second)
-         (first))))
+  (let [[_ items] (feed/fetch-items url)
+        data (->> items
+                  (remove :content)
+                  (map #(future (find-content %)))
+                  (map deref)
+                  (filter second))]
+    (when (not-empty data)
+      {:content-ident (compute-content-identifier data)
+       :content-to-summary-ratio (compute-content-to-summary-ratio data)})))
 
-(defn write-content-ident [index url ident]
+(defn write-stat [index url stat]
   (let [dir (get-in index [url :dir])]
     (storage/set-attrs dir
-                       (assoc (storage/get-attrs dir)
-                              :content-ident ident)))
+                       (merge (storage/get-attrs dir) stat)))
   index)
 
 (defn collect-feed! [url]
-  (let [ident (compute url)]
-    (send feed/feed-index write-content-ident url ident)
-    ident))
+  (let [stat (compute url)]
+    (send feed/feed-index write-stat url stat)
+    stat))
 
 (defn collect-and-log-safe! [url]
   (try
     (log/info "Collecting statistics for" url)
-    (let [ident (collect-feed! url)]
-      (log/info "Content identifier for" url "is" ident)
-      ident)
+    (let [stat (collect-feed! url)]
+      (log/info "Statistics for" url "are" stat)
+      stat)
     (catch Exception ex
       (log/error ex "Failed to collect statistics for" url))))
 
