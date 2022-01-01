98b9d0fe4d31de9181ec94ffb835bf5e7e779cf0
Georgy Kibardin <georgy@kibardin.name>
2022-01-01 17:57:11 +0300

    Snapshot 0.23

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject feedcircuit-revisited "0.22"
+(defproject feedcircuit-revisited "0.23-SNAPSHOT"
   :description "Old friend Feedcircuit revisited"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
