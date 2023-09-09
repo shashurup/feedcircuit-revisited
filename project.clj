ac247318709bd3cf89661005e32d4dfb8784a3d4
Georgy Kibardin <georgy@kibardin.name>
2023-09-09 11:18:40 +0300

    Starting 0.29 snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject feedcircuit-revisited "0.28"
+(defproject feedcircuit-revisited "0.29-SNAPSHOT"
   :description "Old friend Feedcircuit revisited"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
