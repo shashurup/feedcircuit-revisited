ee354ca1e2c56d3f1024032f07bfb387c65bc7a0
Georgy Kibardin <georgy@kibardin.name>
2024-04-30 20:47:59 +0300

    Release 0.30

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject feedcircuit-revisited "0.30-SNAPSHOT"
+(defproject feedcircuit-revisited "0.30"
   :description "Old friend Feedcircuit revisited"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
