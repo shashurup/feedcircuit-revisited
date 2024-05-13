eeb46565bb1be6cb3cc279309638637da2ca7320
Georgy Kibardin <georgy@kibardin.name>
2024-05-13 18:44:34 +0300

    Release 0.32

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject feedcircuit-revisited "0.32-SNAPSHOT"
+(defproject feedcircuit-revisited "0.32"
   :description "Old friend Feedcircuit revisited"
   :url "http://feedcircuit.kibardin.name"
   :min-lein-version "2.0.0"
