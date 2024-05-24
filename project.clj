6d2202fb28c637f21d82fd48d16d98338936b057
Georgy Kibardin <georgy@kibardin.name>
2024-05-24 17:03:44 +0300

    Starting 36 snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject feedcircuit-revisited "35"
+(defproject feedcircuit-revisited "36-snapshot"
   :description "Old friend Feedcircuit revisited"
   :url "http://feedcircuit.kibardin.name"
   :min-lein-version "2.0.0"
